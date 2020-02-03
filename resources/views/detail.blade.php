fbd2bd46593f8d770574bf9730f515646e8d9ffa
志谷優介 <>
2020-02-03 18:11:26 +0900

    目安達成時間用に.c-timeクラスを作成

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -17,7 +17,7 @@
     {{ $step['name'] }}
   </h2>
   <p class="c-time">
-    目安達成時間： {{ $step['time'] ? $step['time'] : '未入力' }}
+    目安達成時間： {{ $step['time'] ? $step['time'] : '未設定' }}
   </p>
   <div class="p-detail">
     <div class="p-detail__content">
