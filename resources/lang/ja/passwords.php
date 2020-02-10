ca88c039611893a2a44451590df3166f810e26ac
志谷優介 <>
2020-02-10 22:56:00 +0900

    もっかい

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードは8文字以上でご入力ください',
+    'reset' => 'パスワードがリセットされました',
+    'sent' => 'ご指定のアドレス宛にメールが送信されました。',
+    'token' => '不具合が発生しました。時間をおいて再度お試しください。',
+    'user' => "メールアドレスが誤っています"
+
+];
