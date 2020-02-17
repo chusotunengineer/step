0e962f09a720c4bf1e7f3e232420972560b9701f
志谷優介 <>
2020-02-17 18:01:17 +0900

    修正

--- public/js/app.js
+++ public/js/app.js
@@ -23871,7 +23871,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _vm.items.steps.length
+    _vm.items.steps.length !== 0
       ? _c(
           "div",
           { staticClass: "u-display--flex" },
